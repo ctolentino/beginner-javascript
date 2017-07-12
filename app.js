6303e3787344ecb92ed510a1477fbc00cef071bd
Chenelle Tolentino <ctolentino@myhta.org>
2017-07-11 15:31:36 -1000

    final 7-11

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-function fortuneTeller(){
-
+function fortuneTeller(childNum, PartnerName, geo, jobTitle){
+return "you will be a " + jobTitle + " in " + geo + " and married to " + PartnerName+ " with " + childNum + " kids.";
+alert( "you will be a " + jobTitle + " in " + geo + " and married to " + PartnerName+ " with " + childNum + " kids.");
 }
 
-function ageCalculator(){
+var myFortune = fortuneTeller(3, "bob", "Hawaii", "Developer");
+console.log(myFortune);
 
+function ageCalculator(birthYear, futureYear){
+var possibleAge1 = futureYear - birthYear;
+var possibleAge2 = (futureYear - birthYear) + 1;
+return "I will be either " + possibleAge1 + " or " + possibleAge2 + " in " + futureYear;
+alert( "you will be a " + jobTitle + " in " + geo + " and married to " + PartnerName+ " with " + childNum + " kids.");
 }
+
+var myAge = ageCalculator(1986, 2020);
+console.log(myAge);
